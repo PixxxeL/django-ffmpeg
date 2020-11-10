7835b9e6d6c35fcfd38b0fdebe6cdf92a38e3c3a
Ivan Sergeev <ivan.n.sergeev@gmail.com>
2020-11-10 12:30:23 +0500

    Make necessary directories

--- django_ffmpeg/app.py
+++ django_ffmpeg/app.py
@@ -1,7 +1,28 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+
+from django_ffmpeg import defaults as ffmpeg_settings
 
 
 class DjangoFfmpegConfig(AppConfig):
     name = 'django_ffmpeg'
     verbose_name = _('Videos')
+
+    def ready(self):
+        self._make_directories()
+
+    def _make_directories(self):
+        dirs = (
+            ffmpeg_settings.FFMPEG_ORIG_VIDEO,
+            ffmpeg_settings.FFMPEG_THUMB_VIDEO,
+            ffmpeg_settings.FFMPEG_CONV_VIDEO,
+        )
+        for _ in dirs:
+            os.makedirs(os.path.join(
+                settings.MEDIA_ROOT,
+                ffmpeg_settings.FFMPEG_PRE_DIR,
+                _
+            ), exist_ok =True)
