30d22fba756a41f0982754c8c1d133b5e48eadfb
pixxxel <ivan.n.sergeev@gmail.com>
2020-11-17 15:44:38 +0500

    Fix celery task if video already converting

--- django_ffmpeg/tasks.py
+++ django_ffmpeg/tasks.py
@@ -1,13 +1,25 @@
+import logging
+
 from celery.task import task
 
-from django_ffmpeg.models import Video, ConvertingCommand
+from django_ffmpeg.models import (
+    Video, ConvertingCommand,
+    VIDEO_CONVERSION_STATUS_CHOICES as STATUSES
+)
 from django_ffmpeg.utils import Converter
+
+
+logger = logging.getLogger(__name__)
 
 
 @task
 def convert_video(command_id, video_id):
     command = ConvertingCommand.objects.get(pk=command_id)
     video = Video.objects.get(pk=video_id)
+    started = STATUSES[1][0]
+    if video.convert_status in [started]:
+        logger.error('Video already converting')
+        return
     converter = Converter()
     converter.convert_video_thumb(command, video)
     converter.convert_video_file(command, video)
